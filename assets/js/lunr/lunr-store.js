bcc07f813598e96de0272e4da03c77beeb0a6d0c
Sidd Karamcheti <skaramcheti@cs.stanford.edu>
2021-06-21 16:40:13 -0700

    Site updated at: 2021-06-21 23:40:12 UTC

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "The Annotated Bottom-Up Top-Down Attention for VQA",
+        "excerpt":"Hello, World!  ","categories": ["Annotated-ML"],
+        "tags": ["vqa","pytorch"],
+        "url": "https://siddkaramcheti.com/blog/annotated-butd/",
+        "teaser":null}]
